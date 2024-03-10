5499492ef667016cdee4b1742e6f0cb126bfcadb
강민희 <100061907+minhee810@users.noreply.github.com>
2024-03-10 17:06:03 +0900

    [Bronze I] Title: 평균, Time: 252 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -7,18 +7,19 @@ public class Main {
         int N = sc.nextInt(); // 3
         int[] A = new int[N]; // 길이가 3인 배열 생성
 
-        for (int i = 0; i < A.length; i++) { // 길이가 3인 배열에 입력되는 성적 저장
-            A[i] += sc.nextInt(); // A 배열에 입력되는 값 저장하기
-        }
+//        for (int i = 0; i < A.length; i++) { // 길이가 3인 배열에 입력되는 성적 저장
+//            A[i] += sc.nextInt(); // A 배열에 입력되는 값 저장하기
+//        }
 
         long sum = 0;
         long max = 0;
 
         for (int i = 0; i < A.length; i++) {
-            if (A[i] > max) {
-                max = A[i];
+            int tmp = sc.nextInt();
+            if (tmp > max) {
+                max = tmp;
             }
-            sum += A[i];
+            sum += tmp;
         }
 
         System.out.println(sum*100.0/max/N);
