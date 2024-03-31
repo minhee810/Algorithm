52b774c2610031cbb89aa597530ec08728db87d9
minhee Kang <100061907+minhee810@users.noreply.github.com>
2024-04-01 01:54:41 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,46 +1,42 @@
 class Solution {
     public String solution(String code) {
-        String ret = "";
-        
-        int mode = 0;
-        int len = code.length();
-        char[] codeArr = code.toCharArray();
         
         if (code.isEmpty()) {
             return "EMPTY";
         }
 
-        
-        for(int i = 0; i < len; i++){
-           
-            if(mode == 0){
-                
-                if(codeArr[i] != '1'){
-                    if(i%2 == 0){
-                        ret += codeArr[i];
-                    }
-                }else{
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            char currentChar = code.charAt(i);
+
+            if (mode == 0) {
+
+                if (currentChar != '1' && i % 2 == 0) {
+                    ret.append(currentChar);
+                } else if (currentChar == '1') {
                     mode = 1;
                 }
-            
-            }else if(mode == 1){
-                
-                if(codeArr[i] != '1'){ //5
-                    if(i%2 == 1){
-                        ret += codeArr[i];
-                    }
-                }else{
+
+            } else {
+
+                if (currentChar != '1' && i % 2 != 0) { //5
+
+                    ret.append(currentChar);
+
+                } else if (currentChar == '1') {
                     mode = 0;
                 }
-                
+
             }
-       }
-    
-        if(ret.equals("")){
-            ret = "EMPTY";
         }
 
-        
-        return ret;
+        if (ret.length() == 0) {
+            return "EMPTY";
+        }
+
+
+        return ret.toString();
     }
 }
